d6097bca6db396c40e113d32a2233b841cbccb05
Fuzier <63809340+Fuzier@users.noreply.github.com>
2020-05-10 18:06:11 +0300

    Add files via upload

---
+++ Hot-potato.py
@@ -0,0 +1,37 @@
+import discord
+from discord.ext import commands
+from discord.ext.commands import Bot
+import time
+import asyncio
+import os
+import sys
+
+interval = 3
+timer = 6
+zad = 60 * 5
+
+currentTime = time.time()
+
+Bot = commands.Bot(command_prefix= '.')
+
+take = ['поймал', 'словил']
+commands = ['.картошка']
+
+@Bot.event
+async def on_ready():
+    print("Bot is online")
+
+@Bot.command( pass_context = True )
+async def картошка(ctx, member = ''):
+    channel = Bot.get_channel(707981325591773215)
+
+    await channel.send("Таймер на 5 минут начался, быстрее пиши \".поймал\"!")
+    await asyncio.sleep(300)
+    await channel.send("К сожалению ты проиграл :(")
+@Bot.command( pass_context = True )
+async def поймал(ctx):
+    channel = Bot.get_channel(707981325591773215)      
+    await channel.send("Молодец! Ты продолжаешь учавствовать в игре :)")
+    os.execl(sys.executable, sys.executable, *sys.argv) 
+    
+Bot.run('NzA3OTQ1NTY2NjQxODQ4Mzkw.XrQLxg.KxU0heEJsQQNmsceDGmZCj80ghk')
